53af6249317b4382ee0b35ec6b8a6aed6b6496d0
Mercy Cherono <cheronom142@gmail.com>
2023-09-27 22:31:24 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Calling the reduce()method on the input array and passing it the arguments, result and number.
+//The callback function iterates through the array, adding the batch numbers to the result that has a start value of 0.
+//The function returns the sum of all assembled batteries at the end of the iteration.
+const totalBatteries = batteryBatches.reduce(function (result, number) {
+  return result + number;
+}, 0);
